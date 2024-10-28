43b1e99bca55ce20bf43505f5581f667930017ea
Engineeer-ravi <pathakravi.er@gmail.com>
2024-10-28 14:27:21 +0530

    code

---
+++ String/precticeQs2.js
@@ -0,0 +1,3 @@
+let fullName =prompt("enter your Fullname without space");
+let username="@"+fullName+fullName.length;
+console.log(username);
---
+++ String/testString.js
@@ -0,0 +1,45 @@
+// let str="Ravi pathak";
+// let str2='Soni'
+// console.log(str2[1]);
+
+//Template Litrels
+// let specialString ='this is a template litrels ${1+2+3+4}';
+// console.log( specialString);
+
+// let obj ={
+//     item:"pen",
+//     price:10,
+// };
+
+// console.log("the cost of",obj.item,"is",obj.price,"rupees");
+
+//template Littrel next line correcter
+//console.log("Ravi\nPatahak");
+
+//tab space
+//console.log("Ravi\tPatahak");
+
+
+//let str="Ravi\tPatahk"
+//console.log(str.length);
+// let str="  Ravi Pathak  ";
+
+//delet chrecceter
+
+// let str="hello";
+// console.log(str.slice(0,3));
+
+//concat Methode
+// let str1="Ravi";
+// let str2="Pathak";
+// let str3="Code";
+// let res =str1+str2+str3;
+// console.log(res);
+
+//replace methode
+// let str='RaviPAtahk';
+// console.log(str.replace("R","A"))
+
+//chardecter find out
+// let str="ILoveJs";
+// console.log(str.charAt(5));
