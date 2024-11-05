4f947da9c2c3746697e6015ec721f1d317d6eb68
Engineeer-ravi <pathakravi.er@gmail.com>
2024-11-05 23:35:27 +0530

    code

--- 01_variable.js
+++ 01_variable.js
@@ -1,3 +1,17 @@
+// const accountId =14556;
+// let accountEmail="ravi123@gmail.com";
+// var accountPassword= "12345";
+// accountCity="Amabala";
+
+
+//  accountEmail="pathakravi.er@gmail.com";
+//  accountpassword ="12345";
+//  accountCity ="CHANDIGARH";
+// console.log(accountId);
+
+//  console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
 const accountId =14556;
 let accountEmail="ravi123@gmail.com";
 var accountPassword= "12345";
---
+++ Function And Methode/script.js
@@ -0,0 +1,39 @@
+// function myFunction(){
+//     console.log("Welcome to Webdevlopment ");
+//     console.log("We are Learning JS")
+
+// }
+// myFunction();
+
+// function rFunction(msg){
+//     // parameter >>Input
+//     console.log(msg)
+
+// }
+//  rFunction("i Love Js");  //argument
+
+//function 2 number sum
+// function sum(a,b){
+//     //local Variable
+//     // console.log(a+b)
+//     s=a+b;
+//     console.log(a+b)
+//     return s;
+// }
+//  let  value =sum(3,5);
+//  console.log(a+b);
+//  console.log(value);
+
+//  Arrow funnction
+// function sum(a,b){
+//     return a+b;
+
+//}
+const arrowSum=(a,b) =>{
+    console.log(a+b);
+};
+//multiplication funcion
+const arrowMul=(a,b) =>{
+    console.log(a*b);
+
+};
